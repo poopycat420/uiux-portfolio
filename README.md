f285012d810ce3caa214249c3e6068935c0744be
Riki Fameli <114783766+poopycat420@users.noreply.github.com>
2022-12-06 12:58:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uiux-portfolio
+ 
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello test its me
+    </div>
+  )
+}
+
+export default App
